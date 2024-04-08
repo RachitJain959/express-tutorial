5119e46b5865af378daefc17a805b9c93e774640
Rachit Jain <rachitjain9596@gmail.com>
2024-04-08 17:01:40 +0530

    methods-post-postman

--- 11-methods.js
+++ 11-methods.js
@@ -13,6 +13,16 @@ app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
 app.post("/api/people", (req, res) => {
   const { name } = req.body;
   if (!name) {
